92262d51ce7627ca5cd0cf3a70e27d20345586b4
Joakim Jansson <joakimxjansson@gmail.com>
2025-05-26 01:35:18 +0200

    changes

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -2,7 +2,7 @@
 import { Button } from "@/components/ui/button"
 import { ChevronDownIcon } from '@heroicons/react/24/outline';
 
-import { useEffect } from 'react';
+
 
 export default function Hero() {
     const scrollToNextSection = () => {
